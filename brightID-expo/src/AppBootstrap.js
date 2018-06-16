eb4ab9b0990830742c25c0e2ec06d9e71168ce8f
Finnegan <dwisner6@gmail.com>
2018-06-16 13:35:45 -0500

    added brightID-expo for testing / developing with the expo client

---
+++ brightID-expo/src/AppBootstrap.js
@@ -0,0 +1,95 @@
+// @flow
+
+import * as React from 'react';
+import {
+	ActivityIndicator,
+	AsyncStorage,
+	StatusBar,
+	StyleSheet,
+	View,
+} from 'react-native';
+import { Font, Permissions } from 'expo';
+import store from './store';
+import { setupPPKeys, generatePPKeys } from './actions/ppKeys';
+import { setUpDefault } from './actions/setUpDefault';
+
+type Props = {
+	navigation: { navigate: Function },
+};
+
+export default class AppBootstrap extends React.Component<Props> {
+	componentDidMount() {
+		this.bootstrapAsync();
+	}
+
+	// Fetch the token from storage then navigate to our appropriate place
+	bootstrapAsync = async () => {
+		// bootstrap the application
+		// async storage key 'userData' : {
+		// userToken: String,
+		// nameornym: String,
+		// avatarUri: String
+		// }
+
+		try {
+			// add user permssions
+			const cam = await Permissions.getAsync(Permissions.CAMERA);
+			const camr = await Permissions.getAsync(Permissions.CAMERA_ROLL);
+			console.warn(cam);
+			console.warn(camr);
+			// load font
+			await Font.loadAsync({
+				EurostileRegular: require('../assets/fonts/EurostileRegular.ttf'),
+				'ApexNew-Book': require('../assets/fonts/ApexNew-Book.otf'),
+				'ApexNew-Medium': require('../assets/fonts/ApexNew-Medium.otf'),
+				'ApexNew-Light': require('../assets/fonts/ApexNew-Light.otf'),
+			});
+
+			// load user data
+			let userData = await AsyncStorage.getItem('userData');
+			if (userData !== null) {
+				userData = JSON.parse(userData);
+				store.dispatch(setUpDefault(userData));
+			} else {
+				store.dispatch(setUpDefault({}));
+			}
+			this.props.navigation.navigate(userData ? 'App' : 'Onboarding');
+		} catch (err) {
+			console.warn(err);
+		}
+
+		// This should check the async storage for the Public/private keys as well.
+		// If they don't exist, it generates new ones using tweetnacl.js.
+
+		try {
+			// Should use some kind of encrypted storage
+			let ppKeys = await AsyncStorage.getItem('connectionPPKeys');
+
+			if (ppKeys !== null) {
+				ppKeys = JSON.parse(ppKeys);
+				store.dispatch(setupPPKeys(ppKeys));
+			} else {
+				// Generate new PPKeys and exchange with server.
+				store.dispatch(generatePPKeys());
+			}
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	// Render any loading content that you like here
+	render() {
+		return (
+			<View style={styles.container}>
+				<ActivityIndicator />
+				<StatusBar barStyle="default" />
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+});
---
+++ brightID-expo/src/actions/index.js
@@ -0,0 +1,103 @@
+// @flow
+
+export const TRUST_SCORE = 'TRUST_SCORE';
+export const CONNECTIONS_COUNT = 'CONNECTIONS_COUNT';
+export const GROUPS_COUNT = 'GROUPS_COUNT';
+export const SEARCH_PARAM = 'SEARCH_PARAM';
+export const ALL_CONNECTIONS = 'ALL_CONNECTIONS';
+export const USER_AVATAR = 'USER_AVATAR';
+export const SAVE_DATA_SUCCESS = 'SAVE_DATA_SUCCESS';
+export const LOADING_USER = 'LOADING_USER';
+export const USER_DATA = 'USER_DATA';
+export const ERROR = 'ERROR';
+export const REMOVE_USER_DATA = 'REMOVE_USER_DATA';
+export const SET_PPKEYS = 'SET_PPKEYS';
+export const REFRESH_NEARBY_PEOPLE = 'REFRESH_NEARBY_PEOPLE';
+
+/**
+ * Redux boilerplate, pass data through the app
+ * Async actions / async functions can be implemented
+ *
+ * @param type derived from constants
+ * @param payload data passed into the redux reducer
+ *
+ */
+
+export const handleError = (error: string) => ({
+  type: ERROR,
+  error,
+});
+
+export const trustScore = (payload: string) => ({
+  type: TRUST_SCORE,
+  payload,
+});
+
+export const groupsCount = (payload: number) => ({
+  type: GROUPS_COUNT,
+  payload,
+});
+
+export const setSearchParam = (value: string) => ({
+  type: SEARCH_PARAM,
+  value,
+});
+
+export const allConnections = (connections: Array<{}>) => ({
+  type: ALL_CONNECTIONS,
+  connections,
+});
+
+export const userAvatar = (avatarUri: String) => ({
+  type: USER_AVATAR,
+  avatarUri,
+});
+
+export const loadingUser = () => ({
+  type: LOADING_USER,
+});
+
+export const removeUserData = () => ({
+  type: REMOVE_USER_DATA,
+});
+export const refreshNearbyPeople = (nearbyPeople: Array<{}>) => ({
+  type: REFRESH_NEARBY_PEOPLE,
+  nearbyPeople,
+});
+
+export const setUserData = ({
+  userToken,
+  nameornym,
+  avatarUri,
+}: {
+  userToken: string,
+  nameornym: string,
+  avatarUri: string,
+}) => ({
+  type: USER_DATA,
+  userToken,
+  nameornym,
+  avatarUri,
+});
+
+export const saveDataSuccess = ({
+  userToken,
+  nameornym,
+  avatarUri,
+}: {
+  userToken: string,
+  nameornym: string,
+  avatarUri: string,
+}) => ({
+  type: SAVE_DATA_SUCCESS,
+  nameornym,
+  avatarUri,
+  userToken,
+});
+
+// @greenkode - ({ppKeys}) uses object destructring so I changed
+//              the parameter to (ppKeys) so that it works correctly
+export const setPPKeys = (ppKeys: {}) => ({
+  type: SET_PPKEYS,
+  ppKeys,
+});
