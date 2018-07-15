5040f56faf3ed8dbeb99377253aceea77c734099
Finnegan <dwisner6@gmail.com>
2018-07-15 14:54:33 -0500

    settings work for both android and ios!!!

--- BrightID-rn/android/app/src/main/java/com/brightid/MainApplication.java
+++ BrightID-rn/android/app/src/main/java/com/brightid/MainApplication.java
@@ -4,8 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.tradle.react.UdpSocketsModule;
-import com.peel.react.TcpSocketsModule;
 import com.bitgo.randombytes.RandomBytesPackage;
 import com.peel.react.rnos.RNOSModule;
 import com.imagepicker.ImagePickerPackage;
@@ -31,8 +29,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new VectorIconsPackage(),
-            new UdpSocketsModule(),
-            new TcpSocketsModule(),
             new RandomBytesPackage(),
             new RNOSModule(),
             new ImagePickerPackage(),
