7156863ebb04c87cf679dfce2767b93ddb942df8
Finnegan <dwisner6@gmail.com>
2018-07-26 12:10:36 -0600

    added reactotron

--- BrightID-rn/index.js
+++ BrightID-rn/index.js
@@ -1,6 +1,7 @@
 /** @format */
 
 import { AppRegistry } from 'react-native';
+import './ReactotronConfig';
 import App from './src/App';
 import { name as appName } from './app.json';
 
--- BrightID-rn/src/store/index.js
+++ BrightID-rn/src/store/index.js
@@ -2,17 +2,16 @@
 
 import thunkMiddleware from 'redux-thunk';
 import { applyMiddleware, createStore, compose } from 'redux';
+import Reactotron from 'reactotron-react-native';
 import reducer from '../reducer';
-const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : compose;
-const store = createStore(
+
+console.warn(__DEV__);
+
+const store = Reactotron.createStore(
   reducer,
-  composeEnhancers(applyMiddleware(thunkMiddleware)),
+  compose(applyMiddleware(thunkMiddleware)),
 );
 
-console.log('window', typeof window);
 const unsubscribe = store.subscribe(() => console.log(store.getState()));
 
 export default store;
