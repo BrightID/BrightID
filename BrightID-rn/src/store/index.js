4b8170fa28a03ae2cc1d84e805dca32a337c16da
Finnegan <dwisner6@gmail.com>
2018-08-12 12:42:34 -0500

    added support for react-native-debugger

--- BrightID-rn/src/store/index.js
+++ BrightID-rn/src/store/index.js
@@ -4,7 +4,12 @@ import thunkMiddleware from 'redux-thunk';
 import { applyMiddleware, createStore, compose } from 'redux';
 import reducer from '../reducer';
 
-const store = createStore(reducer, compose(applyMiddleware(thunkMiddleware)));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunkMiddleware)),
+);
 
 // store.subscribe(() => console.log(store.getState()));
 
