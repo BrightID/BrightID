a7186bc3b2b0296ba1e99b6f3192afff9b89eaf5
Finnegan <dwisner6@gmail.com>
2018-11-04 21:08:14 -0600

    removed old files

--- .v056-brightid-rn/appflow.md
+++
@@ -1,7 +0,0 @@
-# Here's the app flow:
-
----
-
-- AppBootstrap.js - loads user data from async storage
-- this determines whether to navigate to the onboarding or app stack
-- the public / private keys are generated in SignUp.js during at the end of the onboarding flow
--- .v056-brightid-rn/index.js
+++
@@ -1,9 +0,0 @@
-/** @format */
-import { AppRegistry, YellowBox } from 'react-native';
-import App from './src/App';
-import { name as appName } from './app.json';
-
-YellowBox.ignoreWarnings(['Module RNOS requires main queue setup']);
-YellowBox.ignoreWarnings(['Module WebRTCModule requires main queue setup']);
-
-AppRegistry.registerComponent(appName, () => App);
