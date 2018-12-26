4776bad804defcba82669e2abda9e707d8415023
adamstallard <adam.stallard@gmail.com>
2018-12-26 02:53:46 -0800

    dotenv was apply the production variable to debug

--- BrightID/src/Api/server.js
+++ BrightID/src/Api/server.js
@@ -1,7 +1,11 @@
 // @flow
 
-import { SEED_URL } from 'react-native-dotenv';
+import { SEED_URL, SEED_URL_DEV } from 'react-native-dotenv';
 import emitter from '../emitter';
+
+if (__DEV__){
+  SEED_URL = SEED_URL_DEV;
+}
 
 class Server {
   constructor(seedURL: string) {
