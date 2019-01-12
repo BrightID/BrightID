b94f3dcdc8abf573c595c90b97ca437c29db4dde
adamstallard <adam.stallard@gmail.com>
2019-01-12 04:43:25 -0800

    improve the name of groups

--- BrightID/src/utils/groups.js
+++ BrightID/src/utils/groups.js
@@ -1,42 +1,88 @@
 import store from '../store';
 
-const STRANGER = {
-  photo: { filename: '' },
-  name: 'Stranger',
+const memberList = (group) => {
+
+  const { safePubKey, photo, name, connections } = store.getState().main;
+  const { founders, knownMembers, isNew } = group;
+
+  const me = {
+    photo,
+    name,
+  };
+
+  const stranger = {
+    photo: { filename: '' },
+    name: 'Stranger',
+  };
+
+  const memberList = [];
+
+  // Prefer founders if the user knows them. For new groups, only founders can
+  // be used. Use "Stranger" if the user doesn't know the other co-founder.
+
+  founders.forEach((founder) => {
+    if (founder === safePubKey) {
+      memberList.push(me);
+    } else {
+      const connection = connections.find(u => u.publicKey === founder);
+      if (connection) {
+        memberList.push(connection);
+      } else if (isNew) {
+        memberList.push(stranger);
+      }
+    }
+  });
+
+  // If the group is new we're done.
+
+  if (!isNew) {
+
+    // Try to find three members known to the user.
+
+    let m = 0;
+    while (memberList.length < 3 && m < knownMembers.length) {
+      let currentKey = knownMembers[m];
+      if (!founders.includes(currentKey)) {
+        const connection = connections.find(u => u.publicKey === currentKey);
+        if (connection) {
+          memberList.push(connection);
+        }
+      }
+      ++m;
+    }
+
+    // If the user doesn't know three members, add the user if in the group.
+
+    if (memberList.length < 3 && !founders.includes(safePubKey) && knownMembers.includes(safePubKey)) {
+      memberList.push(me);
+    }
+  }
+
+  return memberList;
 };
 
-const findConnection = (findKey, connections) =>
-  connections.find(u => u.publicKey === findKey) || STRANGER;
-
 export const groupCirclePhotos = (group) => {
-  const { photo, safePubKey, connections } = store.getState().main;
-  const { knownMembers, founders } = group;
 
-  const memberList = group.isNew ? founders : knownMembers;
+  const { knownMembers } = group;
 
-  const photos = memberList.map((memberKey) => {
-    let foundPhoto;
-    if (safePubKey === memberKey) {
-      foundPhoto = photo;
-    } else {
-      foundPhoto = findConnection(memberKey, connections).photo;
-    }
+  const photos = memberList(group).map((member) => {
+
     // If a founder isn't in knownMembers, that founder hasn't joined yet and
-    // will show as faded in the list.
-    const faded = group.isNew && !knownMembers.includes(memberKey);
-    return { photo: foundPhoto, faded };
+    // their photo will be faded.
+
+    const faded = group.isNew && !knownMembers.includes(member.publicKey);
+
+    return { photo: member.photo, faded };
   });
-  console.log(photos);
   return photos;
 };
 
 export const getGroupName = (group) => {
-  const { safePubKey, name, connections } = store.getState().main;
-  const memberList = group.isNew ? group.founders : group.knownMembers;
-  const names = memberList.map((publicKey) =>
-    publicKey === safePubKey
-      ? name
-      : findConnection(publicKey, connections).name,
+
+  const names = memberList(group).map(member =>
+    member.name.substr(0, 13),
   );
-  return names.map((n) => n.substr(0, 12)).join(', ');
+
+  return names.join(', ');
 };
+
