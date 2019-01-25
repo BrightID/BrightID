01638a4a269b04866e2122883fb66a5f2cdffb59
adamstallard <adam.stallard@gmail.com>
2019-01-25 14:09:09 -0800

    Some platforms support uint8array.join some don't so test for that specifically

--- BrightID/src/utils/encoding.js
+++ BrightID/src/utils/encoding.js
@@ -12,9 +12,15 @@ export function b64ToUint8Array (str){
   // B64.toByteArray might return a Uint8Array, an Array or an Object depending on the platform.
   // Wrap it in Object.values and new Uint8Array to make sure it's a Uint8Array.
   let arr = B64.toByteArray(str);
-  arr = Object.values(arr);
+  if (arr.join){
+    return arr;
+  }
+  const plainArray = Object.values(arr);
   arr = new Uint8Array(arr);
-  return arr;
+  if (arr.join){
+    return arr;
+  }
+  return plainArray;
 }
 
 export function strToUint8Array(str: string) {
