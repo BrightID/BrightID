fb1e6a340169b9d3c140d7a1a5aa7e4a7b0413df
Drew Gregory <djgregny@gmail.com>
2019-11-07 08:46:22 -0800

    qr check

--- BrightID/src/components/NewConnectionsScreens/actions/parseQrData.js
+++ BrightID/src/components/NewConnectionsScreens/actions/parseQrData.js
@@ -3,7 +3,19 @@
 import { setConnectQrData } from '../../../actions/index';
 import { b64ToUint8Array } from '../../../utils/encoding';
 
+/**
+ * Returns whether the string is a valid QR identifier
+ * @param {*} qrString
+ */
+function validQrString(qrString) {
+  return qrString.length >= 42;
+}
+
 export const parseQrData = (qrString: string) => (dispatch: dispatch) => {
+  if (!validQrString(qrString)) {
+    debugger
+    return; // Do nothing
+  }
   const aesKey = qrString.substr(0, 24);
   const uuid = qrString.substr(24, 12);
   const b64ip = `${qrString.substr(36, 6)}==`;
