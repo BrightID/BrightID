58c9966eba556cd79b0ee48c1c2d71ebcc4ce1b4
rnbwd <dwisner6@gmail.com>
2020-02-12 13:51:47 +0700

    android builds properly

--- BrightID/android/app/build_defs.bzl
+++ BrightID/android/app/build_defs.bzl
@@ -1,5 +1,4 @@
 """Helper definitions to glob .aar and .jar targets"""
-
 def create_aar_targets(aarfiles):
     for aarfile in aarfiles:
         name = "aars__" + aarfile[aarfile.rindex("/") + 1:aarfile.rindex(".aar")]
@@ -8,7 +7,6 @@ def create_aar_targets(aarfiles):
             name = name,
             aar = aarfile,
         )
-
 def create_jar_targets(jarfiles):
     for jarfile in jarfiles:
         name = "jars__" + jarfile[jarfile.rindex("/") + 1:jarfile.rindex(".jar")]
--- BrightID/android/app/src/main/java/org/brightid/MainApplication.java
+++ BrightID/android/app/src/main/java/org/brightid/MainApplication.java
@@ -1,39 +1,44 @@
 package org.brightid;
 
 import android.app.Application;
-import androidx.multidex.MultiDexApplication;
 import android.content.Context;
+
 import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
+
+import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends MultiDexApplication implements ReactApplication {
+public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
-      new ReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-          return BuildConfig.DEBUG;
-        }
+    new ReactNativeHost(this) {
+      @Override
+      public boolean getUseDeveloperSupport() {
+        return BuildConfig.DEBUG;
+      }
 
-        @Override
-        protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return packages;
-        }
+    @Override
+    protected List<ReactPackage> getPackages() {
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      return packages;
+    }
 
-        @Override
-        protected String getJSMainModuleName() {
-          return "index";
-        }
-      };
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
@@ -46,7 +51,6 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this); // Remove this line if you don't want Flipper enabled
   }
-
   /**
    * Loads Flipper in React Native templates.
    *
