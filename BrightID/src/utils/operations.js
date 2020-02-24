35e11f5f436cc3b57461fc3f77c144ef0b1b58e6
Siftal <m.khanmohamadzadeh@gmail.com>
2020-02-24 02:46:54 -0500

    remove failed and old operations from the operations list

--- BrightID/src/utils/operations.js
+++ BrightID/src/utils/operations.js
@@ -5,14 +5,20 @@ import fetchUserInfo from '../actions/fetchUserInfo';
 import api from '../Api/BrightId';
 
 export const pollOperations = async () => {
+  const time_fudge = 2 * 60 * 1000; // trace operations for 2 minutes
   try {
     const { operations } = store.getState();
     for (const op of operations) {
       const { state } = await api.getOperationState(op);
-      console.log('state', state);
-      if (state === 'applied') {
+      if (state === 'applied' || state === 'failed') {
         store.dispatch(removeOperation(op));
         store.dispatch(fetchUserInfo());
+        continue;
+      } else if (state !== 'sent') {
+        console.log(`${state} is an invalid state!`);
+      }
+      if (op.timestamp + time_fudge < Date.now()) {
+        store.dispatch(removeOperation(op));
       }
     }
   } catch (err) {
