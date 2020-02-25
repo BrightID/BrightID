ae30150937de1565594f5c28fdc47496000f3f03
rnbwd <dwisner6@gmail.com>
2020-02-25 09:40:11 +0700

    code-review

--- BrightID/src/utils/operations.js
+++ BrightID/src/utils/operations.js
@@ -4,21 +4,22 @@ import { removeOperation } from '../actions';
 import fetchUserInfo from '../actions/fetchUserInfo';
 import api from '../Api/BrightId';
 
+const time_fudge = 2 * 60 * 1000; // trace operations for 2 minutes
+
 export const pollOperations = async () => {
-  const time_fudge = 2 * 60 * 1000; // trace operations for 2 minutes
   try {
     const { operations } = store.getState();
     for (const op of operations) {
       const { state } = await api.getOperationState(op);
-      if (state === 'applied' || state === 'failed') {
+      if (
+        state === 'applied' ||
+        state === 'failed' ||
+        op.timestamp + time_fudge < Date.now()
+      ) {
         store.dispatch(removeOperation(op));
         store.dispatch(fetchUserInfo());
-        continue;
       } else if (state !== 'sent') {
         console.log(`${state} is an invalid state!`);
-      }
-      if (op.timestamp + time_fudge < Date.now()) {
-        store.dispatch(removeOperation(op));
       }
     }
   } catch (err) {
