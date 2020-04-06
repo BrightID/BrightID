69aea09ce91169c4d30d29c504a2894bbd1af9b3
rnbwd <dwisner6@gmail.com>
2020-04-06 14:17:59 +0800

    pull to refresh

--- BrightID/src/components/Notifications/NotificationsScreen.js
+++ BrightID/src/components/Notifications/NotificationsScreen.js
@@ -1,17 +1,48 @@
 // @flow
 
 import * as React from 'react';
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, RefreshControl } from 'react-native';
 import { connect } from 'react-redux';
 import { INVITE_ACTIVE } from '@/utils/constants';
+import fetchUserInfo from '@/actions/fetchUserInfo';
 import NotificationCard from './NotificationCard';
 import InviteCard from './InviteCard';
 
-class NotificationsScreen extends React.Component<Props> {
+type State = {
+  refreshing: boolean,
+};
+
+class NotificationsScreen extends React.Component<Props, State> {
   static navigationOptions = () => ({
     title: 'Notifications',
     headerBackTitleVisible: false,
   });
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      refreshing: false,
+    };
+  }
+
+  componentDidMount() {
+    const { navigation, dispatch } = this.props;
+    navigation.addListener('willFocus', () => {
+      dispatch(fetchUserInfo());
+    });
+  }
+
+  onRefresh = async () => {
+    try {
+      const { dispatch } = this.props;
+      this.setState({ refreshing: true });
+      await dispatch(fetchUserInfo());
+      this.setState({ refreshing: false });
+    } catch (err) {
+      console.log(err.message);
+      this.setState({ refreshing: false });
+    }
+  };
 
   render() {
     const { navigation, notifications, invites } = this.props;
@@ -25,6 +56,12 @@ class NotificationsScreen extends React.Component<Props> {
         <FlatList
           data={notificationData}
           keyExtractor={({ inviteId, msg }, index) => (inviteId || msg) + index}
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.refreshing}
+              onRefresh={this.onRefresh}
+            />
+          }
           renderItem={({ item }) =>
             item.inviteId ? (
               <InviteCard invite={item} />
