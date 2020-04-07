fc61270bd52617591f5d8fc602bb4f1bb1d3c45d
rnbwd <dwisner6@gmail.com>
2020-04-07 15:12:16 +0800

    add aesKey to new invites

--- BrightID/src/utils/invites.js
+++ BrightID/src/utils/invites.js
@@ -7,56 +7,77 @@ import { saveImage } from './filesystem';
 import { INVITE_ACTIVE } from './constants';
 
 export const getInviteInfo = async (invite: invite) => {
-  console.log('getting invite info', invite);
-  const { connections } = store.getState();
-  const conn = connections.find((conn) => conn.id === invite.inviter);
-  if (!conn.aesKey) {
+  try {
+    console.log('getting invite info', invite);
+    const { connections } = store.getState();
+    const conn = connections.find((conn) => conn.id === invite.inviter);
+    if (!conn.aesKey || !invite.data) {
+      return {};
+    }
+    const groupAesKey = CryptoJS.AES.decrypt(invite.data, conn.aesKey).toString(
+      CryptoJS.enc.Utf8,
+    );
+
+    if (!groupAesKey) {
+      return {};
+    }
+
+    // const uuidKey = invite.url.split('/').pop();
+    const res = await fetch(invite.url);
+    const data = await res.text();
+
+    if (!data) {
+      return {};
+    }
+
+    let info = CryptoJS.AES.decrypt(data, groupAesKey).toString(
+      CryptoJS.enc.Utf8,
+    );
+
+    info = JSON.parse(info);
+
+    info.aesKey = groupAesKey;
+
+    console.log('group info', info);
+    return info;
+  } catch (err) {
+    console.log(`error in getting invite info ${err.message}`);
     return {};
   }
-  const groupAesKey = CryptoJS.AES.decrypt(invite.data, conn.aesKey).toString(
-    CryptoJS.enc.Utf8,
-  );
-  // const uuidKey = invite.url.split('/').pop();
-  const res = await fetch(invite.url);
-  const data = await res.text();
-  let info = CryptoJS.AES.decrypt(data, groupAesKey).toString(
-    CryptoJS.enc.Utf8,
-  );
-  info = JSON.parse(info);
-  console.log('group info', info);
-  return info;
 };
 
 export const updateInvites = async (invites: invite[]): Promise<invite[]> => {
-  const { invites: oldInvites = [] } = store.getState();
-  let newInvites = [];
-  for (const invite of invites) {
-    const oldInvite = oldInvites.find(eqProps('inviteId', invite));
-    if (oldInvite) {
-      oldInvite.members = invite.members;
-      newInvites.push(oldInvite);
-    } else {
-      let info;
-      try {
-        info = await getInviteInfo(invite);
-      } catch (err) {
-        console.log('error in getting invite info' + err instanceof Error ? err.message : err);
-        continue;
-      }
-      let filename = '';
-      if (info?.photo) {
-        filename = await saveImage({
-          imageName: invite.id,
-          base64Image: info?.photo,
+  try {
+    const { invites: oldInvites = [] } = store.getState();
+    let newInvites = [];
+    for (const invite of invites) {
+      const oldInvite = oldInvites.find(eqProps('inviteId', invite));
+      if (oldInvite) {
+        oldInvite.members = invite.members;
+        newInvites.push(oldInvite);
+      } else {
+        let info = await getInviteInfo(invite);
+
+        let filename = '';
+
+        if (info.photo) {
+          filename = await saveImage({
+            imageName: invite.id,
+            base64Image: info.photo,
+          });
+        }
+        const newInv = Object.assign(invite, {
+          name: info.name,
+          state: INVITE_ACTIVE,
+          photo: { filename },
+          aesKey: info.aesKey,
         });
+        newInvites.push(newInv);
       }
-      const newInv = Object.assign(invite, {
-        name: info?.name,
-        state: INVITE_ACTIVE,
-        photo: { filename },
-      });
-      newInvites.push(newInv);
     }
+    return newInvites;
+  } catch (err) {
+    console.log(`error in getting invite info ${err.message}`);
+    return [];
   }
-  return newInvites;
 };
