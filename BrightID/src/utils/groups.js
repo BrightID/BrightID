14823467095e621d631de5ceb7c8c2d321a7ed3f
Abram Symons <abramsymons@gmail.com>
2020-04-09 08:14:53 +0430

    clean know members getter

--- BrightID/src/utils/groups.js
+++ BrightID/src/utils/groups.js
@@ -1,60 +1,18 @@
 import store from '@/store';
 
-const knownMembers = (group) => {
+const threeKnownMembers = (group) => {
   const { id, photo, name, connections } = store.getState();
   const { founders, members, isNew } = group;
-  const me = {
+  const connsWithMe = [...connections, {
     photo,
     name,
     id,
-  };
-
-  let list = [];
-
-  // Prefer founders if the user knows them. For new groups, only founders can
-  // be used. Use "Stranger" if the user doesn't know the other co-founder.
-
-  founders.forEach((founder) => {
-    if (founder === id) {
-      list.push(me);
-    } else {
-      const connection = connections.find((u) => u.id === founder);
-      if (connection) {
-        list.push(connection);
-      }
-    }
-  });
-
-  // If the group is new we're done.
-
-  if (!isNew) {
-    // Try to find three members known to the user.
-
-    const connIds = connections.map((u) => u.id);
-    list = list.concat(
-      members
-        .filter((u) => !founders.includes(u) && connIds.includes(u))
-        .map((u) => connections.find((conn) => conn.id === u)),
-    );
-
-    // let m = 0;
-    // while (list.length < 3 && m < members.length) {
-    //   let current = members[m];
-    //   if (!founders.includes(current)) {
-    //     const connection = connections.find((u) => u.id === current);
-    //     if (connection) {
-    //       list.push(connection);
-    //     }
-    //   }
-    //   m += 1;
-    // }
-
-    // If the user doesn't know three members, add the user if in the group.
-
-    if (list.length < 3 && !founders.includes(id) && members.includes(id)) {
-      list.push(me);
-    }
-  }
+  }];
+  let list = (isNew ? founders : members)
+    .map((u) => connsWithMe.find((conn) => conn.id === u))
+    .filter(u => u)
+    .sort((u1, u2) => founders.includes(u1) ? -1 : 1)
+    .slice(0, 3);
 
   return list;
 };
@@ -62,7 +20,7 @@ const knownMembers = (group) => {
 export const groupCirclePhotos = (group) => {
   const { members } = group;
 
-  const photos = knownMembers(group).map((member) => {
+  const photos = threeKnownMembers(group).map((member) => {
     // If a founder isn't in members, that founder hasn't joined yet and
     // their photo will be faded.
 
@@ -75,8 +33,8 @@ export const groupCirclePhotos = (group) => {
 
 export const getGroupName = (group) => {
   return (
-    group.name ||
-    knownMembers(group)
+    // group.name ||
+    threeKnownMembers(group)
       .map((member) => member.name.substr(0, 13))
       .join(', ')
   );
