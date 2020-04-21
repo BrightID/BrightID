d90fc0f0921b674211e2d9367eb841fe8310eaca
rnbwd <dwisner6@gmail.com>
2020-04-21 16:43:49 +0800

    revert router perfs

--- BrightID/index.js
+++ BrightID/index.js
@@ -1,6 +1,5 @@
 /** @format */
 
-import 'react-native-gesture-handler';
 import { AppRegistry, Text, TextInput } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
@@ -8,13 +7,8 @@ import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Octicons from 'react-native-vector-icons/Octicons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
-// eslint-disable-next-line import/no-unresolved
-import { useScreens } from 'react-native-screens';
 import App from './src/App';
 import { name as appName } from './app.json';
-
-// activate react-native-screens
-useScreens();
 
 // Bootstrap fonts
 Ionicons.loadFont();
