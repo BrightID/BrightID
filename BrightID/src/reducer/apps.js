e62d0cd8c79e1096cdd25c0ed2672ca410d10a5e
rnbwd <dwisner6@gmail.com>
2020-04-26 11:56:58 +0800

    prettier fix

--- BrightID/src/reducer/apps.js
+++ BrightID/src/reducer/apps.js
@@ -1,6 +1,12 @@
 // @flow
 
-import { SET_APPS, ADD_APP, REMOVE_APP, RESET_STORE, UPDATE_APP } from '@/actions';
+import {
+  SET_APPS,
+  ADD_APP,
+  REMOVE_APP,
+  RESET_STORE,
+  UPDATE_APP,
+} from '@/actions';
 import { find, propEq } from 'ramda';
 
 const initialState = {
@@ -29,19 +35,17 @@ export const reducer = (state: AppsState = initialState, action: action) => {
       let apps;
       const updatedApp = find(propEq('name', action.op.context))(state.apps);
       if (updatedApp !== undefined) {
-        const removeExisting = ({ name }) => name !== action.op.context;
         if (action.state === 'applied') {
           updatedApp.state = 'applied';
           updatedApp.contextId = action.op.contextId;
           updatedApp.linked = true;
-        } else if (updatedApp.linked && action.result.includes("duplicate")) {
+        } else if (updatedApp.linked && action.result.includes('duplicate')) {
           updatedApp.state = 'applied';
         } else {
           updatedApp.state = 'failed';
         }
-        apps = state.apps
-          .filter(removeExisting)
-          .concat(updatedApp);
+        const removeExisting = ({ name }) => name !== action.op.context;
+        apps = state.apps.filter(removeExisting).concat(updatedApp);
       } else {
         apps = state.apps;
       }
