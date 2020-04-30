046015f28c32af65192a76c6a06d298ca70b1a64
rnbwd <dwisner6@gmail.com>
2020-04-30 11:39:34 +0800

    add notification badge

--- BrightID/src/routes/helpers.js
+++ BrightID/src/routes/helpers.js
@@ -1,14 +1,55 @@
 import * as React from 'react';
+import { Text, View } from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import LinearGradient from 'react-native-linear-gradient';
 
-export const icon = (active, passive) => ({ focused, color, size }) => (
+export const Icon = (active, passive) => ({ focused, color, size }) => (
   <MaterialCommunityIcons
     name={focused ? active : passive}
     size={size}
     color={color}
   />
 );
+
+export const IconWithBadge = (active, passive, badgeCount) => ({
+  focused,
+  color,
+  size,
+}) => {
+  return (
+    <View style={{ width: 24, height: 24, margin: 5 }}>
+      <MaterialCommunityIcons
+        name={focused ? active : passive}
+        size={size}
+        color={color}
+      />
+      {badgeCount > 0 && (
+        <View style={badgeStyle}>
+          <Text style={badgeText}>{badgeCount}</Text>
+        </View>
+      )}
+    </View>
+  );
+};
+
+const badgeStyle = {
+  // On React Native < 0.57 overflow outside of parent will not work on Android, see https://git.io/fhLJ8
+  position: 'absolute',
+  right: -2,
+  top: -1,
+  backgroundColor: 'red',
+  borderRadius: 7,
+  width: 12,
+  height: 12,
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+const badgeText = {
+  color: 'white',
+  fontSize: 10,
+  fontWeight: 'bold',
+};
 
 const headerTitleStyle = {
   fontFamily: 'EurostileRegular',
--- BrightID/src/routes/index.js
+++ BrightID/src/routes/index.js
@@ -3,96 +3,81 @@
 import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { INVITE_ACTIVE } from '@/utils/constants';
 import Apps from './Apps';
 import Connections from './Connections';
 import Groups from './Groups';
 import Home from './Home';
 import Notifications from './Notifications';
 import Onboarding from './Onboarding';
-import { icon } from './helpers';
+import { Icon, IconWithBadge } from './helpers';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const MainTabs = () => (
-  <Tab.Navigator>
-    <Tab.Screen
-      name="Home"
-      component={Home}
-      options={{ tabBarIcon: icon('home', 'home-outline') }}
-    />
-    <Tab.Screen
-      name="Connections"
-      component={Connections}
-      options={{
-        tabBarIcon: icon('account-heart', 'account-heart-outline'),
-      }}
-    />
-    <Tab.Screen
-      name="Groups"
-      component={Groups}
-      options={{
-        tabBarIcon: icon('account-group', 'account-group-outline'),
-      }}
-    />
-    <Tab.Screen
-      name="Notifications"
-      component={Notifications}
-      options={{ tabBarIcon: icon('bell', 'bell-outline') }}
-    />
-    <Tab.Screen
-      name="Apps"
-      component={Apps}
-      options={{ tabBarIcon: icon('flask', 'flask-outline') }}
-    />
-  </Tab.Navigator>
-);
-
-const MainApp = ({ publicKey }: { publicKey?: string }) => (
-  <Stack.Navigator>
-    {publicKey ? (
-      <Stack.Screen
-        name="App"
-        component={MainTabs}
-        options={{ headerShown: false }}
+const MainTabs = () => {
+  const notificationCount = useSelector(
+    ({ user: { notifications }, groups: { invites } }) =>
+      notifications?.length +
+      invites?.filter((invite) => invite.state === INVITE_ACTIVE)?.length,
+  );
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{ tabBarIcon: Icon('home', 'home-outline') }}
       />
-    ) : (
-      <Stack.Screen
-        name="Onboarding"
-        component={Onboarding}
-        options={{ headerShown: false }}
+      <Tab.Screen
+        name="Connections"
+        component={Connections}
+        options={{
+          tabBarIcon: Icon('account-heart', 'account-heart-outline'),
+        }}
       />
-    )}
-  </Stack.Navigator>
-);
+      <Tab.Screen
+        name="Groups"
+        component={Groups}
+        options={{
+          tabBarIcon: Icon('account-group', 'account-group-outline'),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={Notifications}
+        options={{
+          tabBarIcon: IconWithBadge('bell', 'bell-outline', notificationCount),
+        }}
+      />
+      <Tab.Screen
+        name="Apps"
+        component={Apps}
+        options={{ tabBarIcon: Icon('flask', 'flask-outline') }}
+      />
+    </Tab.Navigator>
+  );
+};
 
-export default connect(({ user: { publicKey } }) => ({ publicKey }))(MainApp);
+const MainApp = () => {
+  const publicKey = useSelector((state) => state.user.publicKey);
+  return (
+    <Stack.Navigator>
+      {publicKey ? (
+        <Stack.Screen
+          name="App"
+          component={MainTabs}
+          options={{ headerShown: false }}
+        />
+      ) : (
+        <Stack.Screen
+          name="Onboarding"
+          component={Onboarding}
+          options={{ headerShown: false }}
+        />
+      )}
+    </Stack.Navigator>
+  );
+};
 
-// const prefix = 'brightid://';
-
-// const App = createAppContainer(AppNavigator);
-
-// const MainApp = () => <App ref={setTopLevelNavigator} uriPrefix={prefix} />;
-
-// const headerTitleStyle = {
-//   fontFamily: 'EurostileRegular',
-//   fontWeight: '200',
-//   fontSize: 24,
-// };
-
-// const headerBackground = () => (
-//   <LinearGradient
-//     colors={['#F52828', '#F76B1C']}
-//     style={{ flex: 1, width: '100%' }}
-//   />
-// );
-
-// const defaultNavigationOptions = ({ navigation }) => ({
-//   headerTintColor: '#fff',
-//   headerTitleStyle,
-//   headerBackground,
-//   headerTitleAlign: 'center',
-//   // cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-// });
+export default MainApp;
