6ac9e010ce5b545b89e72bb26241cc3949bbb27e
rnbwd <dwisner6@gmail.com>
2020-05-05 11:52:47 +0800

    add qrcode timer

--- BrightID/src/utils/constants.js
+++ BrightID/src/utils/constants.js
@@ -10,7 +10,15 @@ const DEVICE_SMALL_AREA = 240000;
 export const DEVICE_TYPE =
   WIDTH * HEIGHT <= DEVICE_SMALL_AREA ? 'small' : 'large';
 
+export const DEVICE_LARGE = DEVICE_TYPE === 'large';
+
+export const DEVICE_SMALL = DEVICE_TYPE === 'small';
+
 export const DEVICE_OS = Platform.OS;
+
+export const DEVICE_ANDROID = DEVICE_OS === 'android';
+
+export const DEVICE_IOS = DEVICE_OS === 'ios';
 
 console.log('width, height, area', WIDTH, HEIGHT, WIDTH * HEIGHT);
 console.log('deviceType', DEVICE_TYPE);
