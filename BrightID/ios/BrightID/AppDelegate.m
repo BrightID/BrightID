734846f7d9fbe738b0ac5068fdce011cdace5869
rnbwd <dwisner6@gmail.com>
2020-06-01 15:40:15 +0800

    fix ios for release

--- BrightID/ios/BrightID/AppDelegate.m
+++ BrightID/ios/BrightID/AppDelegate.m
@@ -63,12 +63,4 @@ static void InitializeFlipper(UIApplication *application) {
   return [RCTLinkingManager application:application openURL:url options:options];
 }
 
-- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
- restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
-{
- return [RCTLinkingManager application:application
-                  continueUserActivity:userActivity
-                    restorationHandler:restorationHandler];
-}
-
 @end
