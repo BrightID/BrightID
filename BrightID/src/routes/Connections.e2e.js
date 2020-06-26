b390aee0d9de178ab15de70c683325f53702c025
rnbwd <dwisner6@gmail.com>
2020-06-26 11:16:27 +0800

    update connections.e2e

--- BrightID/src/routes/Connections.e2e.js
+++ BrightID/src/routes/Connections.e2e.js
@@ -1,21 +1,22 @@
 import * as React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Material from 'react-native-vector-icons/MaterialCommunityIcons';
 import ConnectionsScreen from '@/components/Connections/ConnectionsScreen';
 import SortingConnectionsScreen from '@/components/Connections/SortingConnectionsScreen';
+import SearchConnections from '@/components/Connections/SearchConnections';
 import { DEVICE_IOS } from '@/utils/constants';
 import { createFakeConnection } from '@/components/Connections/models/createFakeConnection';
 import { navigate } from '@/NavigationService';
 import backArrow from '@/static/back_arrow.svg';
 import { SvgXml } from 'react-native-svg';
+import { store } from '@/store';
 import { headerOptions } from './helpers';
 
 const Stack = createStackNavigator();
 
 const connectionsScreenOptions = {
   ...headerOptions,
-  // ONLY DIFFERENCE IS DEV IS MISSING
   headerRight: () => (
     <TouchableOpacity
       testID="createFakeConnectionBtn"
@@ -38,6 +39,20 @@ const connectionsScreenOptions = {
       <SvgXml height="20" xml={backArrow} />
     </TouchableOpacity>
   ),
+  headerTitle: () => {
+    const { connections } = store.getState().connections;
+    return connections.length ? (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'center',
+        }}
+      >
+        <SearchConnections sortable={true} />
+      </View>
+    ) : null;
+  },
 };
 
 const Connections = () => (
