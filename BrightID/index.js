0ba75731704f35c387e0946be54941c16bcc208e
rnbwd <dwisner6@gmail.com>
2020-07-03 11:51:11 +0800

    fix android bug / remove unused font

--- BrightID/index.js
+++ BrightID/index.js
@@ -5,22 +5,24 @@ import { AppRegistry, Text, TextInput } from 'react-native';
 import { enableScreens } from 'react-native-screens';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Octicons from 'react-native-vector-icons/Octicons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import { DEVICE_IOS } from '@/utils/constants';
 import App from './src/App';
 import { name as appName } from './app.json';
 
 console.disableYellowBox = true;
 
 // enable react-native-screens
-enableScreens();
+// enableScreens is causing some android devices to crash
+if (DEVICE_IOS) {
+  enableScreens();
+}
 
 // Bootstrap fonts
 Ionicons.loadFont();
 SimpleLineIcons.loadFont();
-MaterialIcons.loadFont();
 MaterialCommunityIcons.loadFont();
 Octicons.loadFont();
 AntDesign.loadFont();
