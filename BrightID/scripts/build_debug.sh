130a535c76b849469a456d26dd9d81f08f3031ab
Michael Bauer <michael@m-bauer.org>
2020-07-15 19:33:43 +0200

    CI: First attempt at automated debug build

---
+++ BrightID/scripts/build_debug.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+touch ./android/app/src/main/assets/index.android.bundle
+react-native bundle --dev false --platform android --entry-file index.js --bundle-output ./android/app/src/main/assets/index.android.bundle --assets-dest ./android/app/src/main/res/
+cd android && ./gradlew assembleDebug && cd ..
