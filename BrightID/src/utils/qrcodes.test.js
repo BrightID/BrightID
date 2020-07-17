f657ceaa63792161f0ba3c3030095b599328c9b0
Michael Bauer <michael@m-bauer.org>
2020-07-17 09:28:34 +0200

    Add testfile

--- BrightID/src/utils/qrcodes.test.js
+++ BrightID/src/utils/qrcodes.test.js
@@ -1,9 +1,31 @@
+import { NativeModules } from 'react-native';
 import { generateChannelData } from '@/utils/channels';
 import { channel_types } from '@/components/NewConnectionsScreens/channelSlice';
+
+// mocking by module name stopped working since RN61, see
+// https://github.com/facebook/react-native/issues/26579#issuecomment-535765528)
+/*
+jest.mock('NativeModules', () => {
+  return {
+    RNRandomBytes: {
+      randomBytes: jest.fn(),
+    },
+  };
+});
+ */
+
+// Try to mock by full path as suggested workaround - Also not working :-(
+jest.mock('react-native/Libraries/NativeModules', () => {
+  return {
+    RNRandomBytes: {
+      randomBytes: jest.fn(),
+    },
+  };
+});
 
 describe('Test qrcode encoding', () => {
   test('encodes and decodes qrdata', async () => {
     const channelData = await generateChannelData(channel_types.SINGLE);
-    expect(true);
+    expect(channelData).toBeDefined();
   });
 });
