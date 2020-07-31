2efaadffc625a3f189a027a895c2e40819aaf383
Michael Bauer <michael@m-bauer.org>
2020-07-31 10:22:44 +0200

    commit encrypted keystore, extend github Action to create Android release apk.

---
+++ BrightID/scripts/decrypt.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# To encrypt a file:
+# > gpg --symmetric --cipher-algo AES256 path/to/your-secret
+
+# Decrypt the keystore with passphrase passed in via Github Action Secret
+gpg --quiet --batch --yes --decrypt --passphrase="$ENCRYPT_PASSWORD" \
+--output ./android/app/triplespeeder.keystore ./android/app/triplespeeder.keystore.gpg
+
+# Decrypt keystore.properties
+gpg --quiet --batch --yes --decrypt --passphrase="$ENCRYPT_PASSWORD" \
+--output ./android/keystore.properties ./android/keystore.properties.gpg
+
+# Decrypt google key for publishing in app store
+# gpg --quiet --batch --yes --decrypt --passphrase="$ENCRYPT_PASSWORD" \
+# --output ./android/app/google-key.json ./android/app/google-key.json.gpg
