a623de302aa9f05d71eda9c7913c4b708e8cb492
Michael Bauer <michael@m-bauer.org>
2020-10-13 17:29:08 +0200

    tests: fix adding fake connections in e2e tests

--- BrightID/e2e/testUtils.js
+++ BrightID/e2e/testUtils.js
@@ -29,7 +29,7 @@ const createFakeConnection = async (doConfirm = true) => {
   // open MyCode screen
   await element(by.id('MyCodeBtn')).tap();
   // make sure SINGLE connection mode is active
-  await expect(element(by.text('One to One '))).toExist();
+  await expect(element(by.id('single-use-code'))).toExist();
   await expect(element(by.id('fakeConnectionBtn'))).toBeVisible();
   await element(by.id('fakeConnectionBtn')).tap();
   // With automatic sync this test fails intermittent, so use explicit waitFor...
