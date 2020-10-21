70b142e0341d752e22fc8e35f17c382ab9586887
rnbwd <dwisner6@gmail.com>
2020-10-21 12:33:59 +0800

    remove console logs

--- BrightID/babel.config.js
+++ BrightID/babel.config.js
@@ -1,21 +1,28 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.ios.js', '.android.js', '.js', '.json'],
-        alias: {
-          '@': './src',
+module.exports = (api) => {
+  console.log('api env prod', api.env('production'));
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.json'],
+          alias: {
+            '@': './src',
+          },
         },
-      },
+      ],
+      [
+        'babel-plugin-inline-import',
+        {
+          extensions: ['.svg'],
+        },
+      ],
+      api.env('production') && [
+        'transform-remove-console',
+        { exclude: ['error'] },
+      ],
     ],
-    [
-      'babel-plugin-inline-import',
-      {
-        extensions: ['.svg'],
-      },
-    ],
-  ],
+  };
 };
