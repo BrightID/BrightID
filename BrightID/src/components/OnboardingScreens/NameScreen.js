ece1883ca9a9cfe5dc15ec307e346f67c8019a72
rnbwd <dwisner6@gmail.com>
2021-01-13 17:06:28 +0800

    style name screen / add routes

---
+++ BrightID/src/components/OnboardingScreens/NameScreen.js
@@ -0,0 +1,152 @@
+// @flow
+
+import React, { useState } from 'react';
+import {
+  Image,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
+import { fontSize } from '@/theme/fonts';
+import { WHITE, BLACK, DARKER_GREY, ORANGE } from '@/theme/colors';
+import { DEVICE_LARGE } from '@/utils/deviceConstants';
+
+/* Description */
+/* ======================================== */
+
+/**
+ * Initial Onboarding screen of BrightID
+ */
+
+/* Onboarding Screen */
+/* ======================================== */
+
+export const NameScreen = () => {
+  // slide item
+  const { t } = useTranslation();
+  const navigation = useNavigation();
+
+  const [name, setName] = useState('');
+  return (
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor={WHITE}
+        animated={true}
+      />
+      <View style={styles.orangeTop} />
+      <View style={styles.container}>
+        <View style={styles.descContainer}>
+          <Text style={styles.registerText}>
+            {t('signup.text.whatsYouName')}
+          </Text>
+        </View>
+        <View style={styles.midContainer}>
+          <TextInput
+            testID="editName"
+            onChangeText={setName}
+            value={name}
+            placeholder={t('signup.placeholder.name')}
+            placeholderTextColor={DARKER_GREY}
+            style={styles.textInput}
+            autoCapitalize="words"
+            autoCorrect={false}
+            textContentType="name"
+            underlineColorAndroid="transparent"
+            blurOnSubmit={true}
+          />
+          <Text style={styles.privacyText}>
+            {t('signup.text.nameNotShared')}
+          </Text>
+        </View>
+        <View style={styles.midContainer}>
+          <TouchableOpacity
+            style={styles.submitBtn}
+            onPress={() => {
+              navigation.navigate('SignupName');
+            }}
+            accessibilityLabel={t('signup.button.submit')}
+          >
+            <Text style={styles.submitBtnText}>
+              {t('signup.button.submit')}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  orangeTop: {
+    backgroundColor: ORANGE,
+    height: DEVICE_LARGE ? 70 : 65,
+    width: '100%',
+    zIndex: 1,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: WHITE,
+    borderTopLeftRadius: 58,
+    marginTop: -58,
+    overflow: 'hidden',
+    zIndex: 2,
+  },
+  descContainer: {
+    marginTop: DEVICE_LARGE ? 70 : 60,
+  },
+  midContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  registerText: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: fontSize[16],
+    textAlign: 'center',
+    lineHeight: DEVICE_LARGE ? 26 : 24,
+    marginTop: DEVICE_LARGE ? 20 : 18,
+  },
+  textInput: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: fontSize[16],
+    color: BLACK,
+    borderBottomWidth: 1,
+    borderBottomColor: DARKER_GREY,
+    width: '60%',
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  privacyText: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: fontSize[11],
+    color: DARKER_GREY,
+    textAlign: 'center',
+    marginTop: DEVICE_LARGE ? 36 : 30,
+  },
+  submitBtn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: DEVICE_LARGE ? 180 : 160,
+    height: DEVICE_LARGE ? 50 : 45,
+    backgroundColor: ORANGE,
+    borderRadius: 100,
+    elevation: 1,
+    shadowColor: BLACK,
+    shadowOffset: { width: 0, height: 4 },
+    shadowRadius: 4,
+  },
+  submitBtnText: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: fontSize[16],
+    color: WHITE,
+  },
+});
+
+export default NameScreen;
---
+++ BrightID/src/components/OnboardingScreens/PhotoScreen.js
@@ -0,0 +1,179 @@
+// @flow
+
+import * as React from 'react';
+import {
+  Image,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { useTranslation } from 'react-i18next';
+import { fontSize } from '@/theme/fonts';
+import { WHITE, BLACK, GREEN, ORANGE } from '@/theme/colors';
+import { DEVICE_LARGE } from '@/utils/deviceConstants';
+import VerifiedBadge from '../Icons/VerifiedBadge';
+
+/* Description */
+/* ======================================== */
+
+/**
+ * Initial Onboarding screen of BrightID
+ */
+
+/* Onboarding Screen */
+/* ======================================== */
+
+export const Onboard = () => {
+  // slide item
+  console.log('DEVICE_LARGE', DEVICE_LARGE);
+  const { t } = useTranslation();
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <StatusBar
+          barStyle="dark-content"
+          backgroundColor={WHITE}
+          animated={true}
+        />
+
+        <View style={styles.header}>
+          <Image
+            source={require('@/static/brightid-final.png')}
+            accessible={true}
+            accessibilityLabel="Home Header Logo"
+            resizeMode="contain"
+            style={styles.logo}
+          />
+        </View>
+
+        <View style={styles.center}>
+          <Image
+            source={require('@/static/brightid-phone.png')}
+            accessible={true}
+            accessibilityLabel="Home Header Logo"
+            resizeMode="contain"
+            style={styles.phone}
+          />
+          <View style={styles.verifiedBadge}>
+            <VerifiedBadge
+              width={DEVICE_LARGE ? 65 : 60}
+              height={DEVICE_LARGE ? 65 : 60}
+              strokeWidth={5.5}
+              color={GREEN}
+            />
+          </View>
+        </View>
+        <Text style={styles.registerText}>{t('onboarding.text.register')}</Text>
+        <View style={styles.btnContainer}>
+          <TouchableOpacity style={styles.createBtn}>
+            <Text style={styles.createBtnText}>
+              {t('onboarding.button.create')}
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.recoverBtn}>
+            <Text style={styles.recoverBtnText}>
+              {t('onboarding.button.recover')}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+      <View style={styles.orangeBottom} />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: WHITE,
+    alignItems: 'center',
+    flexDirection: 'column',
+    borderBottomLeftRadius: 58,
+    borderBottomRightRadius: 58,
+    marginBottom: DEVICE_LARGE ? 35 : 20,
+    zIndex: 2,
+    overflow: 'hidden',
+  },
+  orangeBottom: {
+    backgroundColor: ORANGE,
+    width: '100%',
+    height: 100,
+    zIndex: 1,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  header: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '15%',
+    // borderWidth: 1,
+  },
+  logo: {
+    maxWidth: '40%',
+    maxHeight: 90,
+  },
+  center: {},
+  phone: {
+    width: DEVICE_LARGE ? 140 : 130,
+    maxHeight: DEVICE_LARGE ? 180 : 165,
+  },
+  verifiedBadge: {
+    position: 'absolute',
+    right: 10,
+    bottom: 5,
+  },
+  registerText: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: fontSize[16],
+    textAlign: 'center',
+    lineHeight: DEVICE_LARGE ? 26 : 24,
+    marginTop: DEVICE_LARGE ? 20 : 18,
+  },
+  btnContainer: {
+    width: '85%',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexGrow: 1,
+  },
+  createBtn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: DEVICE_LARGE ? 50 : 45,
+    backgroundColor: ORANGE,
+    borderRadius: 100,
+    elevation: 1,
+    shadowColor: BLACK,
+    shadowOffset: { width: 0, height: 4 },
+    shadowRadius: 4,
+  },
+  createBtnText: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: fontSize[16],
+    color: WHITE,
+  },
+  recoverBtn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: DEVICE_LARGE ? 50 : 45,
+    backgroundColor: WHITE,
+    borderWidth: 1,
+    borderColor: ORANGE,
+    borderRadius: 100,
+    marginTop: DEVICE_LARGE ? 14 : 12,
+  },
+  recoverBtnText: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: fontSize[16],
+    color: ORANGE,
+  },
+});
+
+export default Onboard;
