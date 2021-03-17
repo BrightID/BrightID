b73953b15da060a3041e2ed724e75ad36b60eede
David Wisner <dwisner6@gmail.com>
2021-03-17 11:51:06 +0800

    add loading to homescreen verification info

--- BrightID/src/components/HomeScreen.tsx
+++ BrightID/src/components/HomeScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import {
+  ActivityIndicator,
   Image,
   Linking,
   StyleSheet,
@@ -17,7 +18,7 @@ import { useDispatch, useSelector } from '@/store';
 import { useTranslation } from 'react-i18next';
 import { setActiveNotification } from '@/actions';
 import { retrieveImage } from '@/utils/filesystem';
-import { WHITE, ORANGE, BLACK, BLUE } from '@/theme/colors';
+import { WHITE, ORANGE, BLACK, BLUE, DARKER_GREY } from '@/theme/colors';
 import fetchUserInfo from '@/actions/fetchUserInfo';
 import ChatBox from '@/components/Icons/ChatBox';
 import VerifiedBadge from '@/components/Icons/VerifiedBadge';
@@ -70,13 +71,21 @@ export const HomeScreen = (props) => {
   const verifiedApps = useSelector(verifiedAppsSelector);
   const brightIdVerified = useSelector(brightIdVerifiedSelector);
   const [profilePhoto, setProfilePhoto] = useState('');
+  const [loading, setLoading] = useState(true);
 
   const { t } = useTranslation();
 
   useFocusEffect(
     useCallback(() => {
-      dispatch(fetchUserInfo());
       retrieveImage(photoFilename).then(setProfilePhoto);
+      setLoading(true);
+      dispatch(fetchUserInfo())
+        .then(() => {
+          setLoading(false);
+        })
+        .catch(() => {
+          setLoading(false);
+        });
     }, [dispatch, photoFilename]),
   );
 
@@ -205,6 +214,10 @@ export const HomeScreen = (props) => {
                 Verified for {verifiedAppsCount} app
                 {verifiedAppsCount > 1 ? 's' : ''}
               </Text>
+            </View>
+          ) : loading ? (
+            <View style={styles.verified}>
+              <ActivityIndicator size="small" color={DARKER_GREY} animating />
             </View>
           ) : (
             <View style={styles.verified}>
@@ -402,6 +415,7 @@ const styles = StyleSheet.create({
   },
   verified: {
     marginTop: 8,
+    minWidth: 100,
   },
   verifiedText: {
     fontFamily: 'Poppins-Medium',
