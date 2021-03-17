65add5d13905677ca3b33a053ff9b9c6be8b5c84
David Wisner <dwisner6@gmail.com>
2021-03-17 14:11:12 +0800

    generic detox

---
+++ BrightID/src/utils/Detox.js
@@ -0,0 +1,6 @@
+/*
+  This file enables showing of drop down alerts when running in normal configuration.
+  See also file "Detox.e2e.js".
+ */
+export const DetoxEnabled = false;
+export default DetoxEnabled;
