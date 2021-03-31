9a694e2d89cdbf68ed88c23431ec14ad077bae42
Michael Bauer <michael@m-bauer.org>
2021-03-31 10:59:21 +0200

    Fix: Add missing return statement in task check function
    Should finally fix "Joined connection party" achievement.

--- BrightID/src/components/Tasks/UserTasks.ts
+++ BrightID/src/components/Tasks/UserTasks.ts
@@ -89,10 +89,12 @@ export const UserTasks = {
     description: i18next.t(`achievements.joinConnectionParty.description`),
     url: 'https://www.brightid.org/meet',
     checkFn(state) {
-      state.user.verifications.find(
-        (verification) =>
-          verification.name === 'SeedConnected' &&
-          (verification as SeedConnectedVerification).rank > 0,
+      return Boolean(
+        state.user.verifications.find(
+          (verification) =>
+            verification.name === 'SeedConnected' &&
+            (verification as SeedConnectedVerification).rank > 0,
+        ),
       );
     },
   },
