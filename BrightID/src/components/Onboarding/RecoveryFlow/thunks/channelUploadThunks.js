ffe3b44c307a3308d506bbb5621d7ef152508bb3
Michael Bauer <michael@m-bauer.org>
2021-04-13 14:03:03 +0200

    Fix upload of mutual info during recovery

--- BrightID/src/components/Onboarding/RecoveryFlow/thunks/channelUploadThunks.js
+++ BrightID/src/components/Onboarding/RecoveryFlow/thunks/channelUploadThunks.js
@@ -111,10 +111,12 @@ const uploadGroup = async ({ group, channelApi, aesKey }) => {
   }
 };
 
-export const uploadMutualInfo = ({ conn, aesKey, channelApi }) => async (
-  dispatch,
-  getState,
-) => {
+export const uploadMutualInfo = ({
+  conn,
+  aesKey,
+  channelApi,
+  nodeApi,
+}) => async (dispatch, getState) => {
   try {
     const dataIds = await channelApi.list(hash(aesKey));
     if (!dataIds.includes(`connection_${conn.id}`)) {
@@ -130,7 +132,10 @@ export const uploadMutualInfo = ({ conn, aesKey, channelApi }) => async (
     connections = _.keyBy(connections, 'id');
     groups = _.keyBy(groups, 'id');
 
-    const otherSideConnections = await api.getConnections(conn.id, 'inbound');
+    const otherSideConnections = await nodeApi.getConnections(
+      conn.id,
+      'inbound',
+    );
     const knownLevels = ['just met', 'already known', 'recovery'];
     const mutualConnections = otherSideConnections
       ? otherSideConnections
@@ -148,7 +153,7 @@ export const uploadMutualInfo = ({ conn, aesKey, channelApi }) => async (
       mutualConnections.push(user);
     }
 
-    const otherSideGroups = await api.getUserInfo(conn.id)?.groups;
+    const otherSideGroups = await nodeApi.getUserInfo(conn.id)?.groups;
     const mutualGroups = otherSideGroups
       ? otherSideGroups.filter((g) => groups[g.id]).map((g) => groups[g.id])
       : [];
