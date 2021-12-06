a7665965030f11a0822680f01e307974ede19d29
Michael Bauer <michael@m-bauer.org>
2021-12-06 15:41:51 +0100

    Dont check for codepush updates automatically when in DEV mode

--- BrightID/codepush.config.js
+++ BrightID/codepush.config.js
@@ -1,7 +1,9 @@
 import codePush from 'react-native-code-push';
 
 export default {
-  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+  checkFrequency: __DEV__
+    ? codePush.CheckFrequency.MANUAL
+    : codePush.CheckFrequency.ON_APP_RESUME,
   installMode: codePush.InstallMode.ON_NEXT_RESUME,
   rollbackRetryOptions: {
     delayInHours: 1,
