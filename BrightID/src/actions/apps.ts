2bb8a7f86e31daf20b00f81d2de9e1245e28b246
Michael Bauer <michael@m-bauer.org>
2021-12-13 16:10:27 +0100

    bump version 4.9.0

--- BrightID/src/actions/apps.ts
+++ BrightID/src/actions/apps.ts
@@ -40,8 +40,11 @@ export const updateBlindSigs =
           continue;
         }
 
+        let t1, t2, t3, t4;
+
         try {
-          const network = __DEV__ ? 'test' : 'node';
+          let startTime = Date.now();
+          const network = /* __DEV__ ? 'test' : */ 'node';
           // noinspection HttpUrlsUsage
           const url = app.nodeUrl || `http://${network}.brightid.org`;
           const api = new NodeApi({ url, id, secretKey });
@@ -51,12 +54,16 @@ export const updateBlindSigs =
             // eslint-disable-next-line no-continue
             continue;
           }
+          t1 = Date.now() - startTime;
+          startTime = Date.now();
           const client = new WISchnorrClient(wISchnorrPublic);
           const pub = await api.getPublic(
             app.id,
             roundedTimestamp,
             verification,
           );
+          t2 = startTime - Date.now();
+          startTime = Date.now();
           const uid = CryptoJS.enc.Base64.stringify(
             CryptoJS.lib.WordArray.random(16),
           );
@@ -71,6 +78,8 @@ export const updateBlindSigs =
             info,
             uid,
           );
+          t3 = startTime - Date.now();
+          startTime = Date.now();
           console.log(challenge, 'challenge');
           console.log(info, 'info');
           const s = stringify({ id, public: pub });
@@ -83,17 +92,14 @@ export const updateBlindSigs =
             sig,
             challenge.e,
           );
+          t4 = startTime - Date.now();
+          startTime = Date.now();
           console.log(response, 'response');
           const blindSig = client.GenerateWISchnorrBlindSignature(
             challenge.t,
             response,
           );
           console.log('final sig', blindSig);
-          if (!client.VerifyWISchnorrBlindSignature(blindSig, info, uid)) {
-            console.log(`wrong signature for ${app.name} (${verification})!`);
-            // eslint-disable-next-line no-continue
-            continue;
-          }
 
           dispatch(
             addSig({
@@ -114,6 +120,11 @@ export const updateBlindSigs =
           Alert.alert(
             `error in getting sig for ${app.name} (${verification})`,
             (err as Error).message,
+          );
+        } finally {
+          Alert.alert(
+            `Timing:`,
+            `T1: ${t1} - T2: ${t2} - T3: ${t3} - T4: ${t4}`,
           );
         }
       }
