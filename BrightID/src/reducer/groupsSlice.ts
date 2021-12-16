19ee790c0f362f588dc4be6afc6e54b604b73fa5
Michael Bauer <michael@m-bauer.org>
2021-12-16 22:39:38 +0100

    Clean up duplicate groups when leaving a group

--- BrightID/src/reducer/groupsSlice.ts
+++ BrightID/src/reducer/groupsSlice.ts
@@ -70,10 +70,24 @@ const groupsSlice = createSlice({
       }
     },
     leaveGroup(state, action: PayloadAction<Group>) {
-      const group = state.groups.find(
+      // get group to leave (and all potential duplicates, see below)
+      const groupsToLeave = state.groups.filter(
         (group) => group.id === action.payload.id,
       );
-      group.state = 'dismissed';
+      for (const group of groupsToLeave) {
+        group.state = 'dismissed';
+      }
+      if (groupsToLeave.length > 1) {
+        // There was a bug that could result in users having the same group multiple times in state.
+        // To clean this up: When leaving a group we look for duplicates and only keep the first matching group in
+        // state "dismissed". The duplicates will be completely removed from state.
+        const groupsWithoutDuplicates = state.groups.filter(
+          (group) => group.id !== action.payload.id,
+        );
+        // only keep first of the duplicates
+        groupsWithoutDuplicates.push(groupsToLeave[0]);
+        state.groups = groupsWithoutDuplicates;
+      }
     },
     dismissFromGroup(
       state,
