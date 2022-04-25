663e3056c45e02941b3dd92e8d8660c23f924ecb
TripleSpeeder <michael@m-bauer.org>
2022-04-25 09:22:47 +0200

    limit number of connections displayed

--- BrightID/src/utils/constants.ts
+++ BrightID/src/utils/constants.ts
@@ -30,6 +30,7 @@ export const RECOVERY_COOLDOWN_DURATION = 7 * 24 * 60 * 60 * 1000; // 1 week
 export const RECOVERY_COOLDOWN_EXEMPTION = 24 * 60 * 60 * 1000; // 24 hours
 export const PROFILE_VERSION = 1;
 export const POSSIBLE_DUPLICATE_STRING_SIMILARITY_RATE = 0.6;
+export const MAX_DISPLAY_CONNECTIONS = 200;
 
 // Channel info
 export const CHANNEL_INFO_NAME = 'channelInfo.json';
