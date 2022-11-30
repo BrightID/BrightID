aa5dd9d1cd178d8f006e1ca145925452d89ff2e1
TripleSpeeder <michael@m-bauer.org>
2022-11-30 11:17:32 +0100

    Disable automatic codepush update for testing

--- BrightID/codepush.config.js
+++ BrightID/codepush.config.js
@@ -3,7 +3,7 @@ import codePush from 'react-native-code-push';
 export default {
   checkFrequency: __DEV__
     ? codePush.CheckFrequency.MANUAL
-    : codePush.CheckFrequency.ON_APP_RESUME,
+    : codePush.CheckFrequency.MANUAL,
   installMode: codePush.InstallMode.ON_NEXT_RESUME,
   rollbackRetryOptions: {
     delayInHours: 1,
