4dc06cb71430d062ce70d1f867124688827bb312
TripleSpeeder <michael@m-bauer.org>
2022-12-11 13:14:28 +0100

    Show last api response or error info when sponsoring times out

--- BrightID/src/components/Apps/appThunks.ts
+++ BrightID/src/components/Apps/appThunks.ts
@@ -281,11 +281,13 @@ export const waitForAppSponsoring =
     const intervalId = setInterval(async () => {
       const timeElapsed = Date.now() - startTime;
       let sponsorshipInfo: SponsorshipInfo | undefined;
+      let errorResponse;
       try {
         sponsorshipInfo = await getSponsorship(appUserId, api);
       } catch (error) {
         console.log(`Error getting sponsorship info:`);
         console.log(`${error}`);
+        errorResponse = error;
       }
       if (sponsorshipInfo) {
         console.log(
@@ -307,7 +309,18 @@ export const waitForAppSponsoring =
       if (timeElapsed > SPONSOR_WAIT_TIME) {
         console.log(`Timeout waiting for sponsoring!`);
         clearInterval(intervalId);
-        dispatch(setLinkingAppError(`Timeout waiting for sponsoring!`));
+        let lastResult;
+        if (sponsorshipInfo) {
+          lastResult = `Last poll result: "appHasAuthorized": "${sponsorshipInfo.appHasAuthorized}", "spendRequested": "${sponsorshipInfo.spendRequested}"`;
+        } else if (errorResponse) {
+          lastResult = `Error: "${errorResponse?.message || errorResponse}"`;
+        } else {
+          // no sponsorshipInfo but also no error
+          lastResult = `Error: Node has not registered the sponsor request`;
+        }
+        dispatch(
+          setLinkingAppError(`Timeout waiting for sponsoring!. ${lastResult}`),
+        );
       }
     }, SPONSORING_POLL_INTERVAL);
     console.log(`Started pollSponsorship ${intervalId}`);
