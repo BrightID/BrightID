78f810c778b2499e52b82f74886012a19a52c848
TripleSpeeder <michael@m-bauer.org>
2022-12-19 13:11:40 +0100

    add testing-library

--- BrightID/jest.config.js
+++ BrightID/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   setupFiles: [
     './jestSetupFile.js',
