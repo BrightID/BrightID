0f7328dc822d50371b810d1b6366d1ddf20341ba
David Wisner <dwisner6@gmail.com>
2023-05-08 12:53:39 +0800

    configure android for 0.71

--- BrightID/android/app/src/main/java/org/brightid/MainActivity.java
+++ BrightID/android/app/src/main/java/org/brightid/MainActivity.java
@@ -4,6 +4,9 @@ package org.brightid;
 import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends ReactActivity {
 
@@ -14,6 +17,23 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
       return "BrightID";
+    }
+
+    /**
+     * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
+     * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+     * (aka React 18) with two boolean flags.
+     */
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+      return new DefaultReactActivityDelegate(
+          this,
+          getMainComponentName(),
+          // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+          DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
+          // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
+          DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
+          );
     }
 
     //react-native-screens override
