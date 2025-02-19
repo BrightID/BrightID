4d259a8a28f562a6039d0e978ebd0ba07649a7eb
RnbWd <dwisner6@gmail.com>
2025-02-19 22:46:23 +0800

    ios compiles

--- BrightID/babel.config.js
+++ BrightID/babel.config.js
@@ -1,3 +1,27 @@
+const { NODE_ENV } = process.env;
+
+const inProduction = NODE_ENV === 'production';
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    '@babel/plugin-syntax-export-default-from',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+    [
+      'babel-plugin-inline-import',
+      {
+        extensions: ['.svg'],
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ].filter(Boolean), // this will filter any falsy plugins (such as removing transform-remove-console when not in production)
 };
